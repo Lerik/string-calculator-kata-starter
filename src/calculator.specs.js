e71935cf3f92bb3710c1347344fc38e3c089ec5b
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2019-05-13 22:11:50 -0500

    Adding test for the third scenario

--- src/calculator.specs.js
+++ src/calculator.specs.js
@@ -16,9 +16,16 @@ describe('The calculator', () => {
                 calculator.add('').should.equal(0);
             });
         });
+
         describe('WHEN the parameter is one number', () => {
             it('should return the same parameter', () => {
                 calculator.add(5).should.equal(5);
+            });
+        });
+
+        describe('WHEN the parameter have two numbers', () => {
+            it('should return the sum of both numbers', () => {
+                calculator.add('4,10').should.equal(14);
             });
         });
     });
