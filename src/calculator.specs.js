ac23e2be84c92b5507b1dce9609b0082d34ad8c2
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2019-05-13 21:42:22 -0500

    Adding test for empty string parameter

--- src/calculator.specs.js
+++ src/calculator.specs.js
@@ -4,15 +4,16 @@ var Calculator = require('../src/calculator.js');
 var should = require('chai').should();
 
 describe('The calculator', () => {
-    
+    var calculator;
+
     beforeEach(()=> {
-        
+        calculator = new Calculator();
     });
     
     describe('can add some numbers', () => {
         it('should accept an empty string and return 0', () => {
-            
+            var result = calculator.add('');
+            result.should.equal(0);
         });
     });
-   
 });
